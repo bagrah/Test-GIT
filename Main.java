8017910f8c930541c11d9809d5d28c8fb82dbb24
Muhammad Bagas Anugrah <bagas190405@student.ub.ac.id>
2025-02-27 08:00:11 +0700

    Update Main.java: Menambahkan fitur baru

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git! This is a simple Java program.");
+        System.out.println("Hello, Git! This is a simple Java program. TEST");
     }
 }
